de35de3248bdca22558278e0df0c1fe9edc1189e
Long <longcualonglonglong11>
2020-12-26 18:57:16 +0700

    first commit

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -0,0 +1,29 @@
+package org.tensorflow.lite.examples.classification;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void forwardTextActivity(View view) {
+    }
+
+    public void forwardImgActivity(View view) {
+        Intent intent = new Intent(this, ClassifierActivity.class);
+        startActivity(intent);
+    }
+}
